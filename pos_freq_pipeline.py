b183df53615a87b0d328784361d311cb9521e7ce
dmitry-uraev <uraev.dima@yandex.ru>
2021-03-12 17:06:41 +0300

    PosFrequencyPipeline template

---
+++ pos_freq_pipeline.py
@@ -0,0 +1,12 @@
+"""
+Implementation of POSFrequencyPipeline for score ten only.
+"""
+
+
+class POSFrequencyPipeline:
+    def __init__(self, assets, destination):
+        pass
+
+
+if __name__ == "__main__":
+    pass
