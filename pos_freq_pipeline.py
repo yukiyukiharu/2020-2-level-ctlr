304a3d75286daea1c364971b6d84d54d0d57feba
dmitry-uraev <uraev.dima@yandex.ru>
2021-03-12 18:26:30 +0300

    [DEV] Updated after review

--- pos_freq_pipeline.py
+++ pos_freq_pipeline.py
@@ -6,7 +6,3 @@ Implementation of POSFrequencyPipeline for score ten only.
 class POSFrequencyPipeline:
     def __init__(self, assets, destination):
         pass
-
-
-if __name__ == "__main__":
-    pass
